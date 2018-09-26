9c4fa507c58d3130e28115bc48dc00b404fe108e
Ben Winter <ben@appcues.com>
2018-09-26 09:38:22 -0400

    added json for milestones

--- js/scroll.js
+++ js/scroll.js
@@ -1,13 +1,24 @@
 // var controller = new ScrollMagic.Controller();
 
+// // var milestoneFade = function(container, percentScrolled) {
+
+// //     //add logic based on Hank feedback to make some divs (e.g CEO hard skills) come in at 30% instead of 20%
+// //     if (containerPercentScrolled > .5) {
+// //       $(container).animate({ opacity: '1' }, 700, 'easeInExpo');
+// //     }
+// // };
+
 // $(function () {
 
 //   var transitionOne = new ScrollMagic.Scene({
 //     triggerElement: '.milestone',
-//     triggerHook: 'onCenter'
+//     triggerHook: 'onCenter',
+//     duration: "300px"
 //   })
-//   .on("enter", function(e) {
-//     $(this).toggleClass('show');
+//   .on("progress", function(e) {
+//   	if (e > .5) {
+//     	$(this).toggleClass('show');
+// 	}
 //   })
 //   .addTo(controller);
 
