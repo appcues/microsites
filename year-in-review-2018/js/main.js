07c3a5037bc8b7f6c377ed2290a0194bc2c59c5a
Ben Winter <ben@appcues.com>
2018-12-19 19:33:33 -0500

    working on year in review

--- year-in-review-2018/js/main.js
+++ year-in-review-2018/js/main.js
@@ -2,6 +2,9 @@ var appcuesYearInReview2018 = {
 
 
 	startSite: function() {
+		$.get("https://s3-us-west-2.amazonaws.com/appcues-public/microsites/year-in-review/2018/index-webflow.html", function(data){
+			$("body").append(data);
+		});
 	}
 
 };
