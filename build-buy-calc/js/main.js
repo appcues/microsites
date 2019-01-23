362765031811ddaeaaee853583a88b62aa6c680a
Ben Winter <ben@appcues.com>
2019-01-23 18:08:02 -0500

    updates to interface and added math calculations

--- build-buy-calc/js/main.js
+++ build-buy-calc/js/main.js
@@ -20,91 +20,124 @@ var appcuesBuildVsBuyCalc = {
 	    return parseFloat(total, 10).toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString().slice(0,-2);
 	},
 
-	zapData: function() {
-		// var postRequestUrlData = "https://hooks.zapier.com/hooks/catch/118654/l4ahnh/?";
+	updateUrl: function(designerCount, pmCount, developerCount, designerSalary, pmSalary, developerSalary) {
 
-		// var z;
-		// for (z = 0; z < appcuesBuildVsBuyCalc.formData.length; z++) { 
-		//     if(appcuesBuildVsBuyCalc.formData[z] === undefined ) {
-		//     	postRequestUrlData += appcuesBuildVsBuyCalc.formDataLabels[z] + "=&";
-		//     } else {
-		//     	postRequestUrlData += appcuesBuildVsBuyCalc.formDataLabels[z] + "=" + appcuesBuildVsBuyCalc.formData[z] + "&";
-		//     }
-		// }
-
-		// postRequestUrlData += "random_id=" + appcuesBuildVsBuyCalc.randomID;
-
-		// $.post(postRequestUrlData, function(data, status){});
 	},
 
+	processURL: function() {
+
+	},
+
+	calculateValues: function() {
+
+		var workingDays = 261, buildDesignDays = 7, buildPMDays = 8.5, buildDeveloperDays = 25, maintainDesignDays = 1, maintainPMDays = 5, maintainDeveloperDays = 15;
+		var designerCount = $('#designer-count').val(), pmCount = $('#pm-count').val(), developerCount = $('#developer-count').val(), designerSalary = $('#designer-salary').val(), pmSalary = $('#pm-salary').val(), developerSalary = $('#developer-salary').val();
+		var costOutput =  designerCount * (designerSalary/workingDays*buildDesignDays) + pmCount * (pmSalary/workingDays*buildPMDays) + developerCount * (developerSalary/workingDays*buildDeveloperDays);
+		var maintenanceOutput =  designerCount * (designerSalary/workingDays*maintainDesignDays) + pmCount * (pmSalary/workingDays*maintainPMDays) + developerCount * (developerSalary/workingDays*maintainDeveloperDays);
+
+		$('#build-cost').text(appcuesBuildVsBuyCalc.formatCurrency(costOutput)).addClass('active');
+		$('#maintain-cost').text(appcuesBuildVsBuyCalc.formatCurrency(maintenanceOutput)).addClass('active');
+		$('#total-cost').text(appcuesBuildVsBuyCalc.formatCurrency(maintenanceOutput + costOutput)).addClass('active');
+
+		$('#build-days-list').find('.designer-days').text(appcuesBuildVsBuyCalc.setDaysValue(designerCount * buildDesignDays));
+		$('#build-days-list').find('.pm-days').text(appcuesBuildVsBuyCalc.setDaysValue(pmCount * buildPMDays));
+		$('#build-days-list').find('.developer-days').text(appcuesBuildVsBuyCalc.setDaysValue(developerCount * buildDeveloperDays));
+
+		$('#maintenance-days-list').find('.designer-days').text(appcuesBuildVsBuyCalc.setDaysValue(designerCount * maintainDesignDays));
+		$('#maintenance-days-list').find('.pm-days').text(appcuesBuildVsBuyCalc.setDaysValue(pmCount * maintainPMDays));
+		$('#maintenance-days-list').find('.developer-days').text(appcuesBuildVsBuyCalc.setDaysValue(developerCount * maintainDeveloperDays));
+
+	},
+
+	setDaysValue: function(value) {
+		if (value === 1) {
+			return value + " day";
+		} else {
+			return value + " days";
+		}
+	},
+
+
+	setSubmitButton: function() {
+		
+	},
+
+	checkInput: function(element) {
+		if(element.hasClass('input-form-row')){
+			var inputValue = element.find('input').val();
+		} else {
+			var inputValue = element.find('.btn-input.selected').text();
+		}
+
+		if (inputValue.length < 1) {
+			return false;
+		} else {
+			return true;
+		}
+
+	},
 	
 
 	initializeSliders: function() {
 		$('input[type="range"]').click(function(){
 			appcuesBuildVsBuyCalc.changeSalaryValue(this);
 		});
+	},
 
-		$('.salary').on("change paste keyup", function() {
-		   appcuesBuildVsBuyCalc.updateSliderValue(this);
+	selectButton: function(element) {
+		$(element.parentElement).children().removeClass("active").removeClass("selected");
+		$(element).addClass("selected").addClass("active");
+	},
+
+	setButtonClicks: function() {
+		$('.btn-choice').click(function(){
+			appcuesBuildVsBuyCalc.selectButton(this);
+		});
+
+		$('#submit-results').click(function() {
+			appcuesBuildVsBuyCalc.calculateValues();
 		});
 	},
 
+	salaryChangeSlider: function() {
+		$('.salary').on("change paste keyup", function() {
+			var targetSlider = $(this).next('.slider-container').find('.slider');
+			appcuesBuildVsBuyCalc.changeSliderPosition(this.value,targetSlider);
+		});
+	},
 
-	initSlider: function(sliderID,vertical) {
+	changeSliderPosition: function(val,sliderID) {
 		/* setup variables for the elements of our slider */
-		debugger;
-		var thumb = document.getElementById("sliderthumb" + slidernum);
-		var shell = document.getElementById("slidershell" + slidernum);
-		var track = document.getElementById("slidertrack" + slidernum);
-		var fill = document.getElementById("sliderfill" + slidernum);
-		var slider = document.getElementById("slider" + slidernum);
 		
+		var thumb = $(sliderID).parent().children(".sliderthumb")[0];
+		var fill = $(sliderID).parent().children(".sliderfill")[0];
+		var slider = $(sliderID)[0];
+		// debugger;
 		var pc = val/(slider.max - slider.min); /* the percentage slider value */
-		var thumbsize = 14; /* must match the thumb size in your css */
-		var bigval = 250; /* widest or tallest value depending on orientation */
-		var smallval = 40; /* narrowest or shortest value depending on orientation */
-		var tracksize = bigval - thumbsize;
-		var fillsize = 16;
-		var filloffset = 10;
-		var bordersize = 2;
-		var loc = vertical ? (1 - pc) * tracksize : pc * tracksize;
 		
-		// fill.style.opacity = pc + 0.2 > 1 ? 1 : pc + 0.2;
-		
-		// thumb.style.top =  (vertical ? loc : 0) + "px";
-		thumb.style.left = (vertical ? 0 : loc) + "px";
-		// fill.style.top = (vertical ? loc + (thumbsize/2) : filloffset + bordersize) + "px";
-		fill.style.left = (vertical ? filloffset + bordersize : 0) + "px";
-		fill.style.width = (vertical ? fillsize : loc + (thumbsize/2)) + "px";
-		// fill.style.height = (vertical ? bigval - filloffset - fillsize - loc : fillsize) + "px";
-		shell.style.height = (vertical ? bigval : smallval) + "px";
-		shell.style.width = (vertical ? smallval : bigval) + "px";
-		// track.style.height = (vertical ? bigval - 4 : fillsize) + "px"; /* adjust for border */
-		track.style.width = (vertical ? fillsize : bigval - 4) + "px"; /* adjust for border */
-		track.style.left = (vertical ? filloffset + bordersize : 0) + "px";
-		// track.style.top = (vertical ? 0 : filloffset + bordersize) + "px";
+		$(sliderID).parents('.slider-container').parent().children('.salary').val(val);
+
+		thumb.style.left = ((pc > .98) ? "98%" : ((pc * 100) - 1) + "%");
+		fill.style.width = ((pc > .99) ? "99%" : (pc * 100) + "%");
 	},
 	
 
-	setSliderValue: function(val,num,vertical) {
-		debugger;
+	setSliderValue: function(value,elementID) {
 		//change to be the salary input value
-		document.getElementById("slider"+num).value = val;
-		showValue(val,num,vertical);
+		$(elementID).val(value);
+		var sliderID = "#" + elementID;
+		appcuesBuildVsBuyCalc.changeSliderPosition(value,sliderID);
 	},
-
-
 
 	changeSalaryValue: function(slider) {
 		$(slider.parentElement).children('.salary').val(appcuesBuildVsBuyCalc.formatCurrency(slider.value));
 	},
 
 
-
 	updateSliderValue: function(element) {
-			var targetSlider = $(element.parentElement).children('.slider');
-            var nonCurrencyValue = parseFloat(element.value.replace(/[^0-9.-]+/g, ''));
-            $(targetSlider).val(nonCurrencyValue).change();
+		var targetSlider = $(element.parentElement).children('.slider');
+        var nonCurrencyValue = parseFloat(element.value.replace(/[^0-9.-]+/g, ''));
+        $(targetSlider).val(nonCurrencyValue).change();
 	},
 
 	getCookie: function(name) {
@@ -128,55 +161,12 @@ var appcuesBuildVsBuyCalc = {
 	},
 
 	startSite: function() {
-		appcuesBuildVsBuyCalc.initSlider();
-		appcuesBuildVsBuyCalc.setSliderValue(100000,"#designer-salary-slider",false);
-		appcuesBuildVsBuyCalc.setSliderValue(100000,"#pm-salary-slider",false);
-		appcuesBuildVsBuyCalc.setSliderValue(100000,"#developer-salary-slider",false);
-		
-
+		appcuesBuildVsBuyCalc.changeSliderPosition(100000, "#designer-salary-slider", false);
+		appcuesBuildVsBuyCalc.changeSliderPosition(100000, "#pm-salary-slider", false);
+		appcuesBuildVsBuyCalc.changeSliderPosition(100000, "#developer-salary-slider", false);
+		appcuesBuildVsBuyCalc.salaryChangeSlider();
+		appcuesBuildVsBuyCalc.setButtonClicks();
 		appcuesBuildVsBuyCalc.randomID = appcuesBuildVsBuyCalc.createRandomID();
-	},			
-
-
-	setButtonClicks: function() {
-		appcuesBuildVsBuyCalc.setSubmitButton();
-	},
-	
-	setSubmitButton: function() {
-		$("#submit-button").click(function(){
-			// var targetInput = $('#question-' + (appcuesBuildVsBuyCalc.formData.length + 1));
-
-			if (appcuesBuildVsBuyCalc.checkInput(targetInput)) {
-				appcuesBuildVsBuyCalc.addData(targetInput);
-				$('#template-title-content').fadeOut("slow");
-				$('#form-content').fadeOut("slow");
-
-				setTimeout(function() {
-					appcuesBuildVsBuyCalc.submitForm();
-				}, 5000);
-				
-				$('#error-message').hide();	
-			} else {
-				$('#error-message').show();
-			}
-
-			
-		});
-	},
-
-	checkInput: function(element) {
-		if(element.hasClass('input-form-row')){
-			var inputValue = element.find('input').val();
-		} else {
-			var inputValue = element.find('.btn-input.selected').text();
-		}
-
-		if (inputValue.length < 1) {
-			return false;
-		} else {
-			return true;
-		}
-
 	}
 
 
