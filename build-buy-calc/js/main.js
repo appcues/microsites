7f1f710abbad066cbc80c4013a9de32018f1fd4f
Ben Winter <ben@appcues.com>
2019-01-28 15:18:09 -0500

    minor ux updates and functionality

--- build-buy-calc/js/main.js
+++ build-buy-calc/js/main.js
@@ -9,6 +9,14 @@ var appcuesBuildVsBuyCalc = {
 	    return parseFloat(total, 10).toFixed(1).replace(/(\d)(?=(\d{3})+\.)/g, "$1,").toString().slice(0,-2);
 	},
 
+	addNumberCommmas: function(number) { 
+	    return number.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+	},
+
+	removeNumberCommas: function(number) {
+		return (+number.replace(',', ''));
+	},
+
 	updateUrl: function(designerCount, pmCount, developerCount, designerSalary, pmSalary, developerSalary) {
 		var params = {designerCount: designerCount, pmCount: pmCount, developerCount: developerCount, designerSalary: designerSalary, pmSalary: pmSalary, developerSalary: developerSalary};
 		var paramsStr = "?" + $.param( params );
@@ -22,12 +30,12 @@ var appcuesBuildVsBuyCalc = {
 
 	processURL: function() {
 		if(window.location.href.indexOf("?") !== -1) {
-			$('#designer-count').val(appcuesBuildVsBuyCalc.findURLParam("designerCount"));
-			$('#pm-count').val(appcuesBuildVsBuyCalc.findURLParam("pmCount"));
-			$('#developer-count').val(appcuesBuildVsBuyCalc.findURLParam("developerCount"));
-			$('#designer-salary').val(appcuesBuildVsBuyCalc.findURLParam("designerSalary"));
-			$('#pm-salary').val(appcuesBuildVsBuyCalc.findURLParam("pmSalary"));
-			$('#developer-salary').val(appcuesBuildVsBuyCalc.findURLParam("developerSalary"));
+			$('#designer-count').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("designerCount")));
+			$('#pm-count').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("pmCount")));
+			$('#developer-count').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("developerCount")));
+			$('#designer-salary').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("designerSalary")));
+			$('#pm-salary').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("pmSalary")));
+			$('#developer-salary').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.findURLParam("developerSalary")));
 			appcuesBuildVsBuyCalc.changeSliderPosition(appcuesBuildVsBuyCalc.findURLParam("designerSalary"), "#designer-salary-slider", false);
 			appcuesBuildVsBuyCalc.changeSliderPosition(appcuesBuildVsBuyCalc.findURLParam("pmSalary"), "#pm-salary-slider", false);
 			appcuesBuildVsBuyCalc.changeSliderPosition(appcuesBuildVsBuyCalc.findURLParam("developerSalary"), "#developer-salary-slider", false);
@@ -43,7 +51,7 @@ var appcuesBuildVsBuyCalc = {
 	calculateValues: function() {
 
 		var workingDays = 261, buildDesignDays = 7, buildPMDays = 8.5, buildDeveloperDays = 25, maintainDesignDays = 1, maintainPMDays = 5, maintainDeveloperDays = 15;
-		var designerCount = $('#designer-count').val(), pmCount = $('#pm-count').val(), developerCount = $('#developer-count').val(), designerSalary = $('#designer-salary').val(), pmSalary = $('#pm-salary').val(), developerSalary = $('#developer-salary').val();
+		var designerCount = $('#designer-count').val(), pmCount = $('#pm-count').val(), developerCount = $('#developer-count').val(), designerSalary = appcuesBuildVsBuyCalc.removeNumberCommas($('#designer-salary').val()), pmSalary = appcuesBuildVsBuyCalc.removeNumberCommas($('#pm-salary').val()), developerSalary = appcuesBuildVsBuyCalc.removeNumberCommas($('#developer-salary').val());
 		var costOutput =  designerCount * (designerSalary/workingDays*buildDesignDays) + pmCount * (pmSalary/workingDays*buildPMDays) + developerCount * (developerSalary/workingDays*buildDeveloperDays);
 		var maintenanceOutput =  designerCount * (designerSalary/workingDays*maintainDesignDays) + pmCount * (pmSalary/workingDays*maintainPMDays) + developerCount * (developerSalary/workingDays*maintainDeveloperDays);
 
@@ -117,6 +125,8 @@ var appcuesBuildVsBuyCalc = {
 		$("#modify-inputs-btn").click(function(){
 			$('#inputs-disable').css('display', 'none');
 			$("#final-output").css('opacity', '0');
+			$('#email-success-message').hide();
+			$('#email-error-message').hide();
 
 		});
 
@@ -135,8 +145,11 @@ var appcuesBuildVsBuyCalc = {
 	salaryChangeSlider: function() {
 		$('.salary').on("change paste keyup", function() {
 			var targetSlider = $(this).next('.slider-container').find('.slider');
-			appcuesBuildVsBuyCalc.changeSliderPosition(this.value,targetSlider);
+			debugger;
+			appcuesBuildVsBuyCalc.changeSliderPosition(appcuesBuildVsBuyCalc.removeNumberCommas(this.value),targetSlider);
 		});
+
+
 	},
 
 	changeSliderPosition: function(val,sliderID) {
@@ -148,7 +161,7 @@ var appcuesBuildVsBuyCalc = {
 		// debugger;
 		var pc = val/(slider.max - slider.min); /* the percentage slider value */
 		
-		$(sliderID).parents('.slider-container').parent().children('.salary').val(val);
+		$(sliderID).parents('.slider-container').parent().children('.salary').val(appcuesBuildVsBuyCalc.addNumberCommmas(val));
 
 		thumb.style.left = ((pc > .98) ? "98%" : ((pc * 100) - 1) + "%");
 		fill.style.width = ((pc > .99) ? "99%" : (pc * 100) + "%");
@@ -156,14 +169,16 @@ var appcuesBuildVsBuyCalc = {
 	
 
 	setSliderValue: function(value,elementID) {
+		var integerValue = appcuesBuildVsBuyCalc.removeNumberCommas(value);
 		//change to be the salary input value
-		$(elementID).val(value);
+		$(elementID).val(integerValue);
 		var sliderID = "#" + elementID;
-		appcuesBuildVsBuyCalc.changeSliderPosition(value,sliderID);
+		appcuesBuildVsBuyCalc.changeSliderPosition(integerValue,sliderID);
 	},
 
 	changeSalaryValue: function(slider) {
-		$(slider.parentElement).children('.salary').val(appcuesBuildVsBuyCalc.formatCurrency(slider.value));
+		$(slider.parentElement).children('.salary').val(appcuesBuildVsBuyCalc.addNumberCommmas(appcuesBuildVsBuyCalc.formatCurrency(slider.value)));
+
 	},
 
 
